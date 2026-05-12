ce20dd8774831afdb6bf36685f20b619779a14e9
KARIUM <spidermanpusing6@gmail.com>
2026-05-12 23:21:24 +0700

    Create tokenomics.js

---
+++ data/tokenomics.js
@@ -0,0 +1,18 @@
+export const tokenomics = [
+  {
+    title: 'Token',
+    value: 'KARIUM (KRM)'
+  },
+  {
+    title: 'Supply',
+    value: '21,000,000'
+  },
+  {
+    title: 'Consensus',
+    value: 'Sol-Hash PoW'
+  },
+  {
+    title: 'Launch',
+    value: 'Fair Launch'
+  }
+]
